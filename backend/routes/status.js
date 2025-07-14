5e917e6f7311295bf905cfa8ad29a8cf51e00df3
mubasharbinafzal391 <mubashar.afzal@selectasol.com>
2025-07-15 01:13:40 +0500

    admin user auth

--- backend/routes/status.js
+++ backend/routes/status.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "API is running properly",
-    timestamp: new Date()
-  });
+router.get('/user-data', auth, authorizeRoles('user', 'admin'), (req, res) => {
+  res.json({ message: 'Accessible by all users' });
 });
+
 
 module.exports = router;
