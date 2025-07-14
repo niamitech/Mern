4fa3a4030419c9f8200da1941c7969b3f029ea77
ranafahadahmad <rana.fahad@selectasol.com>
2025-07-14 21:05:46 +0500

    Configure environment variables

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Backend API is running');
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
